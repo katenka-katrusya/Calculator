175a4a701d4b0d3888910b27588c6d466b133c1a
SEkaterina <katerina-kozyrenko@yandex.ru>
2022-10-18 12:05:20 +0300

    calc

---
+++ main.py
@@ -0,0 +1,15 @@
+import controller
+import model
+import view
+
+
+model.init_first()
+while True:
+    if model.init_ops():
+        break
+    model.init_second()
+    controller.operation()
+    view.print_total()
+    model.first = model.total
+
+
